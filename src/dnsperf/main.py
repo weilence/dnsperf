2b9d887ec22a2db14c7b2a4d93cf49bdc3562506
Weilence <weilence@163.com>
2024-10-30 23:00:15 +0800

    Reduce packet build time

--- src/dnsperf/main.py
+++ src/dnsperf/main.py
@@ -1,5 +1,7 @@
 import ipaddress
 from multiprocessing import Process, cpu_count
+from subprocess import Popen
+from typing import Type
 from scapy.all import *
 from scapy.layers.inet import *
 from scapy.layers.dns import *
@@ -15,18 +17,9 @@ def cli(ctx, debug):
     ctx.obj["debug"] = debug
 
 
-def src_ip_iter(src_ip: str):
-    network = ipaddress.ip_network(src_ip)
-
-    while True:
-        for ip in network.hosts():
-            yield str(ip)
-
-
-def generate_data(file_path: str, src_ip: str):
+def generate_data(file_path: str):
     list = []
 
-    ip_iter = src_ip_iter(src_ip)
     with open(file_path, "r") as f:
         for line in f:
             if not line.strip():
@@ -36,26 +29,60 @@ def generate_data(file_path: str, src_ip: str):
             qtype = strs[1]
             times = strs[2] if len(strs) > 2 else 1
             for _ in range(int(times)):
-                ip = next(ip_iter)
-                list.append((ip, qname, qtype))
+                list.append({"qname": qname, "qtype": qtype})
 
     return list
 
 
+class DNSQREnumeration(Packet):
+    data: list
+    index: int
+
+    def __init__(self, data: list) -> None:
+        super().__init__()
+
+        self.index = 0
+        self.data = data
+
+    def __next__(self):
+        dnsqr = DNSQR(
+            qname=self.data[self.index]["qname"],
+            qtype=self.data[self.index]["qtype"],
+        )
+
+        self.index += 1
+        if self.index >= len(self.data):
+            self.index = 0
+
+        return dnsqr
+
+    def __iter__(self):
+        return self
+
+    def copy(self):
+        return DNSQREnumeration(self.data)
+
+
 def build_pcap(
     pcap_file: str,
-    data_infos,
+    data,
     src_mac: str,
     dst_mac: str,
+    src_ip: str,
     dst_ip: str,
     ecs: str = None,
 ):
     ether_layer = Ether(src=src_mac, dst=dst_mac)
-    udp_layer = UDP(dport=53)
+    ip_layer = IP(src=RandIP(src_ip), dst=dst_ip)
+    udp_layer = UDP(sport=RandShort(), dport=53)
+    dns_layer = DNS(
+        rd=1,
+        qd=DNSQREnumeration(data),
+    )
 
     if ecs:
         network = ipaddress.ip_network(ecs)
-        dns_ar = DNSRROPT(
+        dns_layer.ar = DNSRROPT(
             rdata=(
                 EDNS0ClientSubnet(
                     family=1,
@@ -65,27 +92,16 @@ def build_pcap(
             )
         )
 
+    packet = ether_layer / ip_layer / udp_layer / dns_layer
+
     with PcapWriter(pcap_file, linktype=1) as pcap_writer:
-        for j, (ip, qname, qtype) in enumerate(data_infos):
-            if j % 10000 == 0 and j != 0:
-                click.echo(f"write {j} packets in {pcap_file}")
+        pcap_writer.write_header(Ether())
+        for i in range(len(data)):
+            if i % 10000 == 0 and i != 0:
+                click.echo(f"write {i} packets in {pcap_file}")
+            pcap_writer.write_packet(packet, sec=0)
 
-            ip_layer = IP(src=ip, dst=dst_ip)
-
-            dns_layer = DNS(
-                rd=1,
-                qd=DNSQR(
-                    qname=qname,
-                    qtype=qtype,
-                ),
-            )
-            if ecs:
-                dns_layer.ar = dns_ar
-
-            packet = ether_layer / ip_layer / udp_layer / dns_layer
-            pcap_writer.write(packet)
-
-    click.echo(f"write {len(data_infos)} packets done in {pcap_file}")
+    click.echo(f"write {len(data)} packets done in {pcap_file}")
 
 
 def merge_pcaps(input_files, output_file):
@@ -105,7 +121,7 @@ def merge_pcaps(input_files, output_file):
 
 
 @cli.command()
-@click.option("-d", "--domain-file", required=True)
+@click.option("-f", "--file", required=True)
 @click.option("--src-mac", required=True)
 @click.option("--dst-mac", required=True)
 @click.option("--src-ip", required=True)
@@ -115,7 +131,7 @@ def merge_pcaps(input_files, output_file):
 @click.pass_context
 def build(
     ctx,
-    domain_file: str,
+    file: str,
     output: str,
     src_mac: str,
     dst_mac: str,
@@ -123,24 +139,25 @@ def build(
     dst_ip: str,
     ecs: str,
 ):
-    data = generate_data(domain_file, src_ip)
+    data = generate_data(file)
     if ctx.obj["debug"]:
         pass
 
     process_list: List[Process] = []
     tmp_file_list: List[str] = []
 
-    cc = cpu_count()
-    for i in range(cc):
-        start = i * len(data) // cc
-        end = (i + 1) * len(data) // cc
+    cpu = cpu_count()
+    for i in range(cpu):
+        start = i * len(data) // cpu
+        end = (i + 1) * len(data) // cpu
         data_list = data[start:end]
 
         tmp_file = f"{output}.{i}"
         tmp_file_list.append(tmp_file)
 
         p = Process(
-            target=build_pcap, args=(tmp_file, data_list, src_mac, dst_mac, dst_ip, ecs)
+            target=build_pcap,
+            args=(tmp_file, data_list, src_mac, dst_mac, src_ip, dst_ip, ecs),
         )
         process_list.append(p)
         p.start()
@@ -156,14 +173,21 @@ def build(
 @click.option("-f", "--pcap-file", required=True)
 @click.option("-i", "--iface", required=True)
 @click.option("--pps", default=1)
-def send(pcap_file: str, iface: str, pps: int):
-    cmd = f"tcpreplay -i {iface} --pps {pps} --loop=0 {pcap_file}"
-    try:
-        click.echo(f"Replaying {pcap_file}...")
-        subprocess.run(cmd, shell=True, check=True)
-        click.echo(f"Successfully replayed {pcap_file}")
-    except subprocess.CalledProcessError as e:
-        click.echo(f"Error replaying pcap: {e}")
+@click.option("--cpu", default=1)
+def send(pcap_file: str, iface: str, pps: int, cpu: int):
+    processes: List[Popen] = []
+    for _ in range(cpu):
+        cmd = f"tcpreplay -i {iface} -K --pps {pps} --loop=0 {pcap_file}"
+        process = subprocess.Popen(cmd, shell=True, check=True)
+        processes.append(process)
+        click.echo(
+            f"send {pcap_file} with {pps} pps on {iface} in process {process.pid}"
+        )
+
+    for p in processes:
+        p.wait()
+
+    click.echo(f"Successfully sent {pcap_file} with {pps} pps on {iface}")
 
 
 if __name__ == "__main__":
